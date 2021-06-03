36da74027820b8d5b493e93dbb0779b756e59e9c
Cleyton Mendes <cleyton.mendest@gmail.com>
2021-06-03 02:48:13 -0300

    Primeiro commit do Site

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// MENU MOBILE DISPLAY
+const btnMobi = document.querySelector('.btn-mobile');
+
+btnMobi.addEventListener('click', () =>{
+    const nav = document.querySelector('.nav');
+     nav.classList.toggle('active');
+})
